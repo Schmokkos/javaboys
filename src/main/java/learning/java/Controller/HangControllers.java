1ea428c6dce60bf5e668efb57b79163d670f3c4f
Joakim Schweizer <joakim.schweizer@nav.no>
2019-02-14 20:07:00 +0100

    Refaktorert metoder i HangControllers

--- src/main/java/learning/java/Controller/HangControllers.java
+++ src/main/java/learning/java/Controller/HangControllers.java
@@ -5,24 +5,19 @@ import learning.java.HangMan;
 public class HangControllers extends HangMan{
 
     public boolean sjekkBokstav(char[] fasitArray,char bokstav){
-        for (int i = 0;i<fasitArray.length;i++){
-            if(fasitArray[i]==bokstav){
+        for (char i:fasitArray){
+            if(i==bokstav){
                 return true;
             }
         }
         return false;
     }
 
-    public boolean harDuVunnet(){
+    public boolean harDuVunnet(char[] array){
 
     }
 
     public boolean harDuTapt(int feilForsøk){
-        if (feilForsøk >= 10){
-            return true;
-        }
-        else {
-            return false
-        }
+        return (feilForsøk >= 10);
     }
 }
